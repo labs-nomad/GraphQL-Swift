b243bf72e55302b758f44b98ae2c59b22fb22cc1
Nomad Company <Nomad@Jons-MacBook-Pro-2.local>
2019-04-22 13:49:07 -0700

    bump 1.0.20

---
+++ GraphQL-Swift/Protocols/GQLFragment.swift
@@ -0,0 +1,26 @@
+//
+//  GQLFragment.swift
+//  GraphQL-Swift
+//
+//  Created by Nomad Company on 4/22/19.
+//  Copyright © 2019 Nomad Company. All rights reserved.
+//
+
+import Foundation
+
+
+/// GraphQL lets you do fragments. This protocol defines what is required of a fragment. Basically just a string literal
+public protocol GQLFragment {
+    /**
+     The literal GraphQL Fragment. It's recommended to use a swift string literal like this:
+     
+     let userDetails = """
+     fragment Details on user {
+        id
+        name
+        email
+     }
+     """
+    **/
+    var fragmentLiteral: String { get }
+}
--- GraphQL-Swift/Protocols/GQLRequest.swift
+++ GraphQL-Swift/Protocols/GQLRequest.swift
@@ -27,56 +27,37 @@ public protocol GQLRequest {
     /// Some GraphQL statements make use of variables. In fact most of them should. This represents the variables.
     var variables: [String: Any]? { get }
     
+    /// Any Fragment that needs to be part of this request.
+    var fragments: [GQLFragment]? { get }
+    
     /// Function that returns a dictionary that represents Swift version of the data that needs to be passed to the server to perform a Graph Query.
     ///
     /// - Returns: A Dictionary in the form of `["query": <myQuery>, "variables": <variables>]`.
     /// - Throws: An error if the dictionary could not be constructed.
     func queryDictionary() throws -> [String: Any]
     
-//    /// The Dictionary that represents the varaibles in a GraphQL Request needs to be converted into a JSON strign. This function accomplishes that.
-//    ///
-//    /// - Returns: The JSON Encoded string or nil if the `varaibles` property is nil.
-//    /// - Throws: An error if the dictionary could not be seralized correctly.
-//    func variablesString() throws -> String?
-    
     /// The Swift dictionary in the form of `["query": <myQuery>, "variables": <variables>]` encoded as data that is ready to be sent in the POST body of the graph QL Request.
     ///
     /// - Returns: The dictionary encoded as `Data`
     /// - Throws: An error if the encoding went wrong.
     func queryData() throws -> Data
-    
-//    /// A network request returns JSON with the results from the request. It's the requests responsability to decode it into it's associated type. Do that is this function.
-//    ///
-//    /// - Parameter data: The results of the request
-//    /// - Returns: An Array of the associated type
-//    /// - Throws: An error if we could not get the associated type from the JSON.
-//    func parseResults(_ data: [String: Any]) throws -> [Result]
-//
-//    /// This associated type should map to the type or types that this request is trying to perform GraphQL on.
-//    associatedtype Result
+
 }
 
 
 public extension GQLRequest {
     func queryDictionary() throws -> [String: Any] {
+        var literal = self.graphQLLiteral
+        
+        for item in self.fragments ?? [] {
+            literal += " \(item.fragmentLiteral)"
+        }
+        
         guard let variables = self.variables else {
             return ["query": "\(self.graphQLLiteral)"]
         }
+        
         return ["query": "\(self.graphQLLiteral)", "variables": variables]
-    }
-    
-    
-    func variablesString() throws -> String? {
-        guard let unwrappedVaraibles = self.variables else {
-            return nil
-        }
-        let variablesData = try JSONSerialization.data(withJSONObject: unwrappedVaraibles, options: .prettyPrinted)
-        
-        guard let variablesString = String(data: variablesData, encoding: .utf8) else {
-            throw GQLRequestError.couldNotConvertToString
-        }
-        
-        return variablesString
     }
     
     func queryData() throws -> Data {
