fe397599dfb52b6955d6c58cf6f42b7d1559ae3a
Nomad Company <Nomad@Jons-MacBook-Pro-2.local>
2019-04-24 16:03:30 -0700

    bump 1.0.30

--- GraphQL-Swift/Controllers/GQLNetworkController.swift
+++ GraphQL-Swift/Controllers/GQLNetworkController.swift
@@ -18,15 +18,13 @@ public class GQLNetworkController: NSObject {
     
     let backgroundSessionKey = "GQLNetworkControllerURLSession"
     
-    var completions: [Int: GQLRequestCompletion] = [:]
-    
     //MARK: Init
     public init(apiDefinition definition: GQLAPIDefinition) {
         self.definition = definition
         super.init()
         let config = URLSessionConfiguration.background(withIdentifier: self.backgroundSessionKey)
         config.isDiscretionary = false
-        self.session = URLSession(configuration: config, delegate: self, delegateQueue: nil)
+        self.session = URLSession(configuration: config, delegate: nil, delegateQueue: nil)
     }
     
     //MARK: Functions
@@ -37,29 +35,24 @@ public class GQLNetworkController: NSObject {
         
         urlRequest.httpBody = try request.queryData()
         
-        let task = self.session.dataTask(with: urlRequest)
-        
-        
-        self.completions[task.taskIdentifier] = completion
-        
-//        let task = self.session.dataTask(with: urlRequest) { (p_data, p_response, p_error) in
-//            if let error = p_error {
-//                completion?(nil, error)
-//            }else if let response = p_response as? HTTPURLResponse, let data = p_data {
-//                switch response.statusCode {
-//                case 200...299:
-//                    guard let json = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: Any] else {
-//                        completion?(nil, nil)
-//                        return
-//                    }
-//                    completion?(json, nil)
-//                default:
-//                    completion?(nil, GQLRequestError.invalidStatusCode(code: response.statusCode))
-//                }
-//            }else {
-//                completion?(nil, GQLRequestError.couldNotParseResponse)
-//            }
-//        }
+        let task = self.session.dataTask(with: urlRequest) { (p_data, p_response, p_error) in
+            if let error = p_error {
+                completion?(nil, error)
+            }else if let response = p_response as? HTTPURLResponse, let data = p_data {
+                switch response.statusCode {
+                case 200...299:
+                    guard let json = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: Any] else {
+                        completion?(nil, nil)
+                        return
+                    }
+                    completion?(json, nil)
+                default:
+                    completion?(nil, GQLRequestError.invalidStatusCode(code: response.statusCode))
+                }
+            }else {
+                completion?(nil, GQLRequestError.couldNotParseResponse)
+            }
+        }
         
         task.resume()
         
